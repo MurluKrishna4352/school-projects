784b2c119a8a45f833383164f6ca601e1c2a4496
Krishna <77536038+MurluKrishna4352@users.noreply.github.com>
2021-08-11 12:19:34 +0530

    print function

---
+++ print function.py
@@ -0,0 +1,6 @@
+# print( ) , its a function to print something
+print("hello")
+#sep operator is used to seperate values insinde a single print statement
+print("Python", "Pool", sep = '          ')# this function is only applied in b/w of the charecters
+#end="" its used only at the end of the charecters 
+print("Python", "Pool", sep = '          ',end='!')
