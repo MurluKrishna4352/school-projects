f22d650df7cfc038674443dd58c0d383d89a8a61
Krishna <77536038+MurluKrishna4352@users.noreply.github.com>
2021-08-13 12:33:30 +0530

    Add files via upload
    dynamically typed language "PYTHON"

---
+++ dynamically typed language.py
@@ -0,0 +1,3 @@
+#python is a dynamically typed language
+#the respect is also given to the charecters and operators
+#there are different id() for different values like the admission number of a student in a school
