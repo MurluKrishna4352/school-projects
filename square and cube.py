af3cfa6718dc1be1d219adeaae91df67579cba06
Krishna <77536038+MurluKrishna4352@users.noreply.github.com>
2021-08-14 17:40:41 +0530

    Add files via upload

---
+++ square and cube.py
@@ -0,0 +1,4 @@
+#Write a Python script to accept a number and display square and cube of a number.
+user_input = float(input("Enter a number to see its square and cube: "))
+print("square of",user_input,': ', round(user_input**2))
+print("cube of",user_input,': ', round(user_input**3))
